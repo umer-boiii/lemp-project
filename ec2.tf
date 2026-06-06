1847115393560b92a525d8f52f6baa8a1eb77190
umer-boiii <i.m.umaribrahim@gmail.com>
2026-06-06 13:44:56 +0000

    style: run terraform fmt to fix pipeline style checks

--- ec2.tf
+++ ec2.tf
@@ -15,12 +15,32 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "php" {
-  ami             = data.aws_ami.ubuntu.id
-  instance_type   = "t3.micro"
-  security_groups = [aws_security_group.TF_SG.name]
-  key_name        = "Haq"
-  user_data       = file("${path.module}/scripts/lemp.sh")
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t3.micro"
+  
+  # Fixed: Swapped to vpc_security_group_ids using the resource ID
+  vpc_security_group_ids = [aws_security_group.TF_SG.id]
+  key_name               = "Haq"
+  
+  # Passes your setup instructions to AWS to run immediately at launch
+  user_data              = file("${path.module}/scripts/lemp.sh")
+
   tags = {
     Name = "PHP Info page"
+  }
+
+  # FIX: This explicitly uploads your whole local scripts folder to /tmp/scripts
+  # so that lemp.sh actually has the 'default' file available when it runs!
+  provisioner "file" {
+    source      = "${path.module}/scripts"
+    destination = "/tmp"
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      # Points to your private key file to authenticate the file copy
+      private_key = file("${path.module}/id_rsa") 
+      host        = self.public_ip
+    }
   }
 }
